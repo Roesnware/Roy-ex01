7991e3966864fd363f9a9fe4de461a48da3e1dc9
Roesn <Bubwub04@icloud.com>
2021-09-05 12:39:59 -0400

    hi

---
+++ src/main/java/Solution01.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution01 {
+    /*
+    Create a program
+    scanner 'input' =  create scanner for user input
+    print "What is your name? "
+    string 'name' = read input from user
+    print "Hello, string from input , nice to meet you!"
+    */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name = input.next();
+        System.out.printf("Hello, %s, nice to meet you! ", name);
+    }
+}
